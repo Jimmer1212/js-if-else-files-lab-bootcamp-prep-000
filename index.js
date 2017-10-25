ab34905f96573e64202f2ae337767d553a049194
James Hodge <james.hodge148@gmail.com>
2017-10-25 18:15:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ if (name === "Susan") {
     console.log("The Name is correct")
 } else {
     console.log("Expected the name to be Susan")
-}
-if (height ==== 74) {
-    console.log("The height is correct")
-}else {
-    console.log("Expected the height to be 74")
-}
+
 
 
 
